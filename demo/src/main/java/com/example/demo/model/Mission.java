bddf461a9fc9736c6c042d2c8d5477366ca3b7fa
benoit.gros-dubois <benoit.gros-dubois@polynom.io>
2025-08-22 12:39:34 +0200

    feat: missions entity repo, model & controller

---
+++ demo/src/main/java/com/example/demo/model/Mission.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Mission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+}
